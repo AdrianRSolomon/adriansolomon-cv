d5b8de9839a32efc984715d5a93880b675a3b327
Adrian.R.Solomon <adriansolomontt@gmail.com>
2023-02-24 22:56:00 -0400

    Added my project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -151,7 +151,7 @@ const Projects = () => {
                 'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
               )}
             >
-              <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-2">
+              <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
                 {posts.map((post) => (
                   <div
                     key={post.id}
